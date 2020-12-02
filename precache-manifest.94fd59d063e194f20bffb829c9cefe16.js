c703bbc95f3cc2000ae4a8cfc0915851c753f4be
yesimnoh <yesimnoh@gmail.com>
2020-12-02 20:28:15 +0900

    Updates

---
+++ precache-manifest.94fd59d063e194f20bffb829c9cefe16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6daea45a45e079fb64b0c1072d819c64",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "861e42b408be99e7ffbe",
+    "url": "/movie_app_2019/static/css/main.05d37451.chunk.css"
+  },
+  {
+    "revision": "4d7c806d0e258b226914",
+    "url": "/movie_app_2019/static/js/2.00a09d16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2019/static/js/2.00a09d16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "861e42b408be99e7ffbe",
+    "url": "/movie_app_2019/static/js/main.8ac4a72f.chunk.js"
+  },
+  {
+    "revision": "059a9ab2adfa282ae316",
+    "url": "/movie_app_2019/static/js/runtime-main.12568eff.js"
+  }
+]);
